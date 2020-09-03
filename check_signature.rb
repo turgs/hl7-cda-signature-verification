0fcbbaa48bdb66c2d7bdb82bd19e273dd61be8f7
Tim <1312348+turgs@users.noreply.github.com>
2020-09-03 18:55:27 +1000

    Create check_signature.rb

---
+++ check_signature.rb
@@ -0,0 +1,9 @@
+#! /bin/ruby
+# this works
+
+cda_sign_file_xml = Nokogiri::XML(cda_sign_filename).remove_namespaces!
+
+expected_root_hash = cda_sign_file_xml.css('signedPayload signedPayloadData eSignature Manifest Reference[URI="CDA_ROOT.XML"] DigestValue')&.text.to_s
+actual_root_hash = Digest::SHA1.base64digest(File.read(cda_root_filename))&.to_s
+
+raise (expected_root_hash == actual_root_hash).inspect
